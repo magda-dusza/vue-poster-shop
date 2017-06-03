84db6335e77c9dca8eb07aa5bdce33e85f1b8840
Magda <magdalena.dusza13@gmail.com>
2017-06-03 13:00:56 +0200

    add item to cart

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
 new Vue ({
     el : '#app',
     data : {
-        total : 0
+        total : 0,
+        items : [
+            { title: 'Item 1', price : 0.99 },
+            { title: 'Item 2', price : 1.59 },
+            { title: 'Item 3', price : 5.97 }
+        ],
+        cart : []
     },
     methods : {
-        addItem : function(){
-            this.total += 9.99;
+        addItem : function(index){
+            this.cart.push(this.items[index]);
+            this.total += this.items[index].price;
         }
     }
 });
