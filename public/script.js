39dd0c3b572c4075e9b252783d62c4678dbc2752
Magda <magdalena.dusza13@gmail.com>
2017-06-04 21:00:34 +0200

    add quantities of items in cart

--- public/script.js
+++ public/script.js
@@ -3,15 +3,29 @@ new Vue ({
     data : {
         total : 0,
         items : [
-            { title: 'Item 1', price : 0.99 },
-            { title: 'Item 2', price : 1.59 },
-            { title: 'Item 3', price : 5.97 }
+            { id:1, title: 'Item 1', price : 0.99 },
+            { id:2, title: 'Item 2', price : 1.59 },
+            { id:3, title: 'Item 3', price : 5.97 }
         ],
         cart : []
     },
     methods : {
         addItem : function(index){
-            this.cart.push(this.items[index]);
+            var position = this.cart.map(function(elem) { return elem.id; }).indexOf(this.items[index].id);
+            var newItem = {};
+
+            if(position > -1) {
+                this.cart[position].qty++;
+            } else {
+                newItem = {
+                    id: this.items[index].id,
+                    title: this.items[index].title,
+                    price: this.items[index].price,
+                    qty: 1
+                }
+                this.cart.push(newItem);
+            }
+
             this.total += this.items[index].price;
         }
     }
